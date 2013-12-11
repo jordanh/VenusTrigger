bd8849ce84e95c56bfc539b5449e7e630f70c0fb
Jordan Husney <jordan.husney@gmail.com>
2013-12-10 22:59:32 -0500

    added comment and copyright

--- VenusTrigger/VenusTrigger.ino
+++ VenusTrigger/VenusTrigger.ino
@@ -1,3 +1,17 @@
+/*
+ VenusTrigger
+
+ A simple sketch to actuate the closure of a
+ venus flytrap leaf using a simple circuit.
+
+ Wait to receive a character on the serial port,
+ then cause pin 2 to go high for 5 seconds. 
+
+ Distributed under the GPLv2 public license.
+
+ Author: jordan.husney@gmail.com
+ (c) 2013
+ */
 
 int incomingByte = 0;
 
